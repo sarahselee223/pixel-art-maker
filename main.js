6e1786901f9a583f7d48faa6d75a1dc6deba8e41
Sarah Lee <sarahselee223@gmail.com>
2018-10-17 16:36:42 -0700

    organize color buttons

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
-const colorArray = ['red', 'orange', 'yellow', 'green', 'skyBlue', 'blue', 'purple','black', 'white']
-let currentColor
 
- colorArray.forEach(function(color) {
-        const buttonSelector = `${color}Button`
-        const currentButton = document.querySelector(`div.btn-group.${buttonSelector}`)
-        currentButton.addEventListener('click', function(even){
-            currentColor = color
-        })
-   });
+let currentColor 
+const colorGrid = document.querySelector('.color-container')
+const colorArray = ['red', 'orange', 'yellow', 'green', 'skyBlue', 'blue', 'purple','black', 'white']
+
+colorArray.forEach(function(color){
+    const colorDiv = document.createElement('div')
+    colorDiv.classList.add('btn-group', `${color}Button`)
+    colorDiv.addEventListener('click', function(event){
+        currentColor = color
+    })
+    colorGrid.appendChild(colorDiv)
+}) 
 
 const grid = document.querySelector('.grid-container')
 for(let i=0; i<1200; i++){
@@ -18,3 +21,4 @@ for(let i=0; i<1200; i++){
         event.target.classList.toggle(currentColor+'Button')
     })
 }
+
